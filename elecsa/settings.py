bee5576d3f91b7a161295b38cc50fa7150092f74
pramath <pramathbnaik@gmail.com>
2022-09-21 07:01:47 +0530

    COMEITED

--- elecsa/settings.py
+++ elecsa/settings.py
@@ -128,8 +128,10 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_DIRS = [
-   os.path.join(BASE_DIR,"static"),
-]
+
+#STATICFILES_DIRS = [
+#   os.path.join(BASE_DIR,"static"),
+#]
+STATIC_ROOT = os.path.join(BASE_DIR,"static")
 import django_heroku
 django_heroku.settings(locals())
